166b65774ce6641b263f54466bf44d612e3985ae
valerioprds <v.paradiso89@gmail.com>
2023-12-01 17:03:06 +0100

    added bootstrap

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MoviesService } from 'src/service/movies.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
+  results: any[] = [];
+  constructor(private moviesService: MoviesService) {}
 
+  performSearch(query: string) {
+    this.moviesService.search(query).subscribe((data) => {
+      this.results = data;
+    });
+  }
 }
