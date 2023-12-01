38d2d3801688d920c2972e24eb0123b7aed89f1d
valerioprds <v.paradiso89@gmail.com>
2023-12-01 16:45:25 +0100

    my first commit

---
+++ src/service/movies.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  private baseUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=893af480'
+
+  constructor(private http: HttpClient) { }
+
+  search(query: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}?search=${query}`);
+  }
+}
