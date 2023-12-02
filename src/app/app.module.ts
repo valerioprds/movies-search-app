475e20b8708efecad0d70e6318a0617372b7cb5c
valerioprds <v.paradiso89@gmail.com>
2023-12-02 19:39:58 +0100

    added filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -17,6 +18,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { FavoritesService } from 'src/service/favorites.service';
 
 @Component({
@@ -8,20 +9,56 @@ import { FavoritesService } from 'src/service/favorites.service';
 })
 export class FavoritesComponent implements OnInit {
   favorites: any[] = [];
+  filteredFavorites: any[] = [];
+  filterForm!: FormGroup;
 
-  constructor(private favoritesService: FavoritesService) { }
+  constructor(private favoritesService: FavoritesService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.loadFavorites();
+    this.initializeForm();
   }
 
   loadFavorites() {
     this.favorites = this.favoritesService.getFavorites();
+    this.filteredFavorites = [...this.favorites];
   }
 
   removeFromFavorites(movie: any) {
     this.favoritesService.removeFavorite(movie);
-    this.loadFavorites(); // Recargar la lista de favoritos después de eliminar
-    console.log('deleted one movie' , this.favorites)
+    this.loadFavorites(); // Reload the favorites list after deletion
+    console.log('deleted one movie', this.favorites);
+  }
+
+  initializeForm() {
+    this.filterForm = this.fb.group({
+      sortType: [''],
+      yearFilter: [''],
+      typeFilter: ['']
+    });
+
+    this.filterForm.valueChanges.subscribe(val => {
+      this.applyFilters();
+    });
+  }
+
+  applyFilters() {
+    let filtered = [...this.favorites];
+
+    if (this.filterForm.value.yearFilter) {
+      filtered = filtered.filter(favorite => favorite.Year === this.filterForm.value.yearFilter);
+    }
+
+    if (this.filterForm.value.typeFilter) {
+      filtered = filtered.filter(favorite => favorite.Type === this.filterForm.value.typeFilter);
+    }
+
+    if (this.filterForm.value.sortType === 'title') {
+      filtered.sort((a, b) => a.Title.localeCompare(b.Title));
+    } else if (this.filterForm.value.sortType === 'year') {
+      filtered.sort((a, b) => a.Year.localeCompare(b.Year));
+    }
+
+    this.filteredFavorites = filtered;
   }
 }
