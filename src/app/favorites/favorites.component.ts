bbe1230a39c785aae9065fd1b0347aad416d5f7e
valerioprds <v.paradiso89@gmail.com>
2023-12-02 20:25:41 +0100

    now can leave rating and comment

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FavoritesService } from 'src/service/favorites.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
 })
 export class FavoritesComponent implements OnInit {
+[x: string]: any;
   favorites: any[] = [];
   filteredFavorites: any[] = [];
   filterForm!: FormGroup;
+  ratingForms: FormGroup[] = [];
+  commentForms: FormGroup[] = [];
 
-  constructor(private favoritesService: FavoritesService, private fb: FormBuilder) { }
+  constructor(
+    private favoritesService: FavoritesService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.loadFavorites();
@@ -22,22 +28,59 @@ export class FavoritesComponent implements OnInit {
   loadFavorites() {
     this.favorites = this.favoritesService.getFavorites();
     this.filteredFavorites = [...this.favorites];
+
+    // Initialize rating and comment forms for each favorite
+    this.filteredFavorites.forEach((favorite) => {
+      const ratingForm = this.fb.group({
+        rating: [
+          favorite.rating || '',
+          [Validators.required, Validators.min(1), Validators.max(5)],
+        ],
+      });
+      const commentForm = this.fb.group({
+        comment: [
+          localStorage.getItem(`comment_${favorite.imdbID}`) || '',
+          Validators.maxLength(200),
+        ],
+      });
+      this.ratingForms.push(ratingForm);
+      this.commentForms.push(commentForm);
+    });
+  }
+
+  saveRating(index: number) {
+    const ratingValue = this.ratingForms[index].get('rating')?.value;
+    localStorage.setItem(
+      `rating_${this.filteredFavorites[index].imdbID}`,
+      ratingValue
+    );
+  }
+  comment: string = ''
+
+  saveComment(index: number) {
+    const commentValue = this.commentForms[index].get('comment')?.value;
+    localStorage.setItem(
+      `comment_${this.filteredFavorites[index].imdbID}`,
+      commentValue
+    );
+
+    this.comment = commentValue
+    console.log(commentValue)
   }
 
   removeFromFavorites(movie: any) {
     this.favoritesService.removeFavorite(movie);
-    this.loadFavorites(); // Reload the favorites list after deletion
-    console.log('deleted one movie', this.favorites);
+    this.loadFavorites();
   }
 
   initializeForm() {
     this.filterForm = this.fb.group({
       sortType: [''],
       yearFilter: [''],
-      typeFilter: ['']
+      typeFilter: [''],
     });
 
-    this.filterForm.valueChanges.subscribe(val => {
+    this.filterForm.valueChanges.subscribe(() => {
       this.applyFilters();
     });
   }
@@ -46,11 +89,15 @@ export class FavoritesComponent implements OnInit {
     let filtered = [...this.favorites];
 
     if (this.filterForm.value.yearFilter) {
-      filtered = filtered.filter(favorite => favorite.Year === this.filterForm.value.yearFilter);
+      filtered = filtered.filter(
+        (favorite) => favorite.Year === this.filterForm.value.yearFilter
+      );
     }
 
     if (this.filterForm.value.typeFilter) {
-      filtered = filtered.filter(favorite => favorite.Type === this.filterForm.value.typeFilter);
+      filtered = filtered.filter(
+        (favorite) => favorite.Type === this.filterForm.value.typeFilter
+      );
     }
 
     if (this.filterForm.value.sortType === 'title') {
